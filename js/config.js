782a892d6f03e5f3d121af154b16491eb90e1ae1
idavailable <104657633+idavailable@users.noreply.github.com>
2026-09-15 13:40:50 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -121,7 +121,42 @@ const API_SITES = {
         name: '🎬魔都动漫',
         detail: 'https://caiji.moduapi.cc'
     },
-
+    ckzy: {
+        api: 'https://ckzy.me/api.php/provide/vod',
+        name: 'CK资源',
+        adult: true
+    },
+    jkun: {
+        api: 'https://jkunzyapi.com/api.php/provide/vod',
+        name: 'jkun资源',
+        adult: true
+    },
+    souav: {
+        api: 'https://api.souavzy.vip/api.php/provide/vod',
+        name: 'souav资源',
+        adult: true
+    },
+    r155: {
+        api: 'https://155api.com/api.php/provide/vod',
+        name: '155资源',
+        adult: true
+    },
+    lsb: {
+        api: 'https://apilsbzy1.com/api.php/provide/vod',
+        name: 'lsb资源',
+        adult: true
+    },
+    huangcang: {
+        api: 'https://hsckzy888.com/api.php/provide/vod',
+        name: '黄色仓库',
+        adult: true,
+        detail: 'https://hsckzy888.com/'
+    },
+    yutu: {
+        api: 'https://apiyutu.com/api.php/provide/vod',
+        name: '玉兔资源',
+        adult: true
+    },
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
 
